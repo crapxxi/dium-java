93d1e40c1e4f6fec93905f5f2eb4fa92d94fb879
crapxxi <aibynkairatov@gmail.com>
2026-03-28 15:49:46 +0500

    [+] add cloud file service

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,6 @@ FROM eclipse-temurin:21-jdk-alpine
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
 
-RUN mkdir -p /app/uploads
-
 EXPOSE 8080
 
 ENTRYPOINT ["java", "-Dspring.profiles.active=prod", "-jar", "app.jar"]
--- src/main/java/com/dium/demo/services/FileService.java
+++ src/main/java/com/dium/demo/services/FileService.java
@@ -1,19 +1,28 @@
 package com.dium.demo.services;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.Map;
 
 @Service
 public class FileService {
-    @Value("${app.upload.dir:/app/uploads/}")
-    private String uploadDir;
+    private final Cloudinary cloudinary;
+
+    public FileService(@Value("${cloudinary.cloud_name}") String cloudName,
+                       @Value("${cloudinary.api_key}") String apiKey,
+                       @Value("${cloudinary.api_secret}") String apiSecret) {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true
+        ));
+    }
 
     public String saveFile(MultipartFile file) throws IOException {
         if (file == null || file.isEmpty()) return null;
@@ -23,30 +32,27 @@ public class FileService {
             throw new RuntimeException("Only images are allowed");
         }
 
-        Path uploadPath = Paths.get(uploadDir);
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
-        }
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
+                "folder", "dium_uploads"
+        ));
 
-        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-        Path filePath = uploadPath.resolve(fileName);
 
-        Files.copy(file.getInputStream(), filePath);
-
-        return "/api/v1/files/" + fileName;
+        return uploadResult.get("url").toString();
     }
 
     public void deleteFile(String fileUrl) {
         if (fileUrl == null || fileUrl.isEmpty()) return;
 
         try {
-            String fileName = fileUrl.replace("/api/v1/files/", "");
-            Path filePath = Paths.get(uploadDir + fileName);
-
-            Files.deleteIfExists(filePath);
-            System.out.println("Файл удален: " + fileName);
+            String publicId = extractPublicId(fileUrl);
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+            System.out.println("Файл удален: " + publicId);
         } catch (IOException e) {
             System.err.println("Ошибка при удалении файла: " + e.getMessage());
         }
+    }
+
+    public String extractPublicId(String fileUrl) {
+        return fileUrl.substring(fileUrl.lastIndexOf("/")  + 1, fileUrl.lastIndexOf("."));
     }
 }
